cb3f3f65032f9828c3bcf8b2f26e941ce5010caf
Miray Karagöz <140845839+miraykaragoz@users.noreply.github.com>
2024-09-02 21:46:06 +0300

    Şehirler

---
+++ wwwroot/js/cities.js
@@ -0,0 +1,152 @@
+﻿let cities = [
+    {
+        name: "Balıkesir",
+        famousFor: "Höşmerim",
+        type: "food",
+        plateNumber: "10"
+    },
+    {
+        name: "Kars",
+        famousFor: "Kaşarı",
+        type: "food",
+        plateNumber: "36"
+    },
+    {
+        name: "Edirne",
+        famousFor: "Ciğer Tava",
+        type: "food",
+        plateNumber: "22"
+    },
+    {
+        name: "Artvin",
+        famousFor: "Karagöl",
+        type: "place",
+        plateNumber: "08"
+    },
+    {
+        name: "Malatya",
+        famousFor: "Kayısı",
+        type: "food",
+        plateNumber: "44"
+    },
+    {
+        name: "Kütahya",
+        famousFor: "Çinileri",
+        type: "place",
+        plateNumber: "43"
+    },
+    {
+        name: "Afyonkarahisar",
+        famousFor: "Sucuk",
+        type: "food",
+        plateNumber: "03"
+    },
+    {
+        name: "Bolu",
+        famousFor: "Yedigöller",
+        type: "place",
+        plateNumber: "14"
+    },
+    {
+        name: "Samsun",
+        famousFor: "Bafra Pidesi",
+        type: "food",
+        plateNumber: "55"
+    },
+    {
+        name: "Kastamonu",
+        famousFor: "Çekme Helva",
+        type: "food",
+        plateNumber: "37"
+    },
+    {
+        name: "Manisa",
+        famousFor: "Mesir Macunu",
+        type: "food",
+        plateNumber: "45"
+    },
+    {
+        name: "Aksaray",
+        famousFor: "Ihlara Vadisi",
+        type: "place",
+        plateNumber: "68"
+    },
+    {
+        name: "Tekirdağ",
+        famousFor: "Köftesi",
+        type: "food",
+        plateNumber: "59"
+    },
+    {
+        name: "Zonguldak",
+        famousFor: "Maden Ocakları",
+        type: "place",
+        plateNumber: "67"
+    },
+    {
+        name: "Elazığ",
+        famousFor: "Harput Kalesi",
+        type: "place",
+        plateNumber: "23"
+    },
+    {
+        name: "Çorum",
+        famousFor: "Leblebi",
+        type: "food",
+        plateNumber: "19"
+    },
+    {
+        name: "Tokat",
+        famousFor: "Tokat Kebabı",
+        type: "food",
+        plateNumber: "60"
+    },
+    {
+        name: "Denizli",
+        famousFor: "Pamukkale",
+        type: "place",
+        plateNumber: "20"
+    },
+    {
+        name: "Ordu",
+        famousFor: "Fındık",
+        type: "food",
+        plateNumber: "52"
+    },
+    {
+        name: "Uşak",
+        famousFor: "Tarihi Evler",
+        type: "place",
+        plateNumber: "64"
+    },
+    {
+        name: "Karaman",
+        famousFor: "Taşkale",
+        type: "place",
+        plateNumber: "70"
+    },
+    {
+        name: "Isparta",
+        famousFor: "Gül",
+        type: "place",
+        plateNumber: "32"
+    },
+    {
+        name: "Bingöl",
+        famousFor: "Yüzen Ada",
+        type: "place",
+        plateNumber: "12"
+    },
+    {
+        name: "Kırklareli",
+        famousFor: "Longoz Ormanları",
+        type: "place",
+        plateNumber: "39"
+    },
+    {
+        name: "Bitlis",
+        famousFor: "Nemrut Krater Gölü",
+        type: "place",
+        plateNumber: "13"
+    }
+];
